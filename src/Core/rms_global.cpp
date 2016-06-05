f10d43dea49ab439da70b69697ac5a8a8c756b85
Bobby Bob <monsieurbobah@yandex.ru>
2016-06-05 14:07:35 +0300

    fix: global translation-aware strings implementation

--- src/Core/rms_global.cpp
+++ src/Core/rms_global.cpp
@@ -1,4 +1,6 @@
 #include <QString>
+#include <QCoreApplication>
+
 #include "rms_global.h"
 
 namespace Rsl {
@@ -6,40 +8,40 @@ namespace MapService {
 
 namespace {
 const char *document_type_strings[] = {
-    QT_TRANSLATE_NOOP("DocumentType", "MAP"),
-    QT_TRANSLATE_NOOP("DocumentType", "ATLAS"),
-    QT_TRANSLATE_NOOP("DocumentType", "GLOBE"),
-    QT_TRANSLATE_NOOP("DocumentType", "BOOK"),
-    QT_TRANSLATE_NOOP("DocumentType", "BIBLIOGRAPHY"),
-    QT_TRANSLATE_NOOP("DocumentType", "PERIODICAL"),
-    QT_TRANSLATE_NOOP("DocumentType", "OPTICALDISC")
+    QT_TRANSLATE_NOOP("Rsl::MapService::DocumentType", "MAP"),
+    QT_TRANSLATE_NOOP("Rsl::MapService::DocumentType", "ATLAS"),
+    QT_TRANSLATE_NOOP("Rsl::MapService::DocumentType", "GLOBE"),
+    QT_TRANSLATE_NOOP("Rsl::MapService::DocumentType", "BOOK"),
+    QT_TRANSLATE_NOOP("Rsl::MapService::DocumentType", "BIBLIOGRAPHY"),
+    QT_TRANSLATE_NOOP("Rsl::MapService::DocumentType", "PERIODICAL"),
+    QT_TRANSLATE_NOOP("Rsl::MapService::DocumentType", "OPTICALDISC")
 };
 
 const char *request_status_strings[] = {
-    QT_TRANSLATE_NOOP("RequestStatus", "FILLING"),
-    QT_TRANSLATE_NOOP("RequestStatus", "PICKUP"),
-    QT_TRANSLATE_NOOP("RequestStatus", "DELIVERED"),
-    QT_TRANSLATE_NOOP("RequestStatus", "DENY_DEPARTMENTS_OCCUPIED"),
-    QT_TRANSLATE_NOOP("RequestStatus", "DENY_EMPLOYEES_OCCUPIED"),
-    QT_TRANSLATE_NOOP("RequestStatus", "DENY_EXHIBITION_OCCUPIED"),
-    QT_TRANSLATE_NOOP("RequestStatus", "DENY_ILES_OCCUPIED"),
-    QT_TRANSLATE_NOOP("RequestStatus", "DENY_STOCK"),
-    QT_TRANSLATE_NOOP("RequestStatus", "DENY_NULL"),
-    QT_TRANSLATE_NOOP("RequestStatus", "RETURNED"),
-    QT_TRANSLATE_NOOP("RequestStatus", "PUT_ON_HOLD")
+    QT_TRANSLATE_NOOP("Rsl::MapService::RequestStatus", "FILLING"),
+    QT_TRANSLATE_NOOP("Rsl::MapService::RequestStatus", "PICKUP"),
+    QT_TRANSLATE_NOOP("Rsl::MapService::RequestStatus", "DELIVERED"),
+    QT_TRANSLATE_NOOP("Rsl::MapService::RequestStatus", "DENY_DEPARTMENTS_OCCUPIED"),
+    QT_TRANSLATE_NOOP("Rsl::MapService::RequestStatus", "DENY_EMPLOYEES_OCCUPIED"),
+    QT_TRANSLATE_NOOP("Rsl::MapService::RequestStatus", "DENY_EXHIBITION_OCCUPIED"),
+    QT_TRANSLATE_NOOP("Rsl::MapService::RequestStatus", "DENY_ILES_OCCUPIED"),
+    QT_TRANSLATE_NOOP("Rsl::MapService::RequestStatus", "DENY_STOCK"),
+    QT_TRANSLATE_NOOP("Rsl::MapService::RequestStatus", "DENY_NULL"),
+    QT_TRANSLATE_NOOP("Rsl::MapService::RequestStatus", "RETURNED"),
+    QT_TRANSLATE_NOOP("Rsl::MapService::RequestStatus", "PUT_ON_HOLD")
 };
 }
 
 QString toString(DocumentType dt)
 {
     Q_ASSERT(DocumentType_MIN <= dt && dt <= DocumentType_MAX);
-    return document_type_strings[dt];
+    return QCoreApplication::translate("Rsl::MapService::DocumentType", document_type_strings[dt]);
 }
 
 QString toString(RequestStatus rs)
 {
     Q_ASSERT(RequestStatus_MIN <= rs && rs <= RequestStatus_MAX);
-    return request_status_strings[rs];
+    return QCoreApplication::translate("Rsl::MapService::RequestStatus", request_status_strings[rs]);
 }
 
 } // namespace MapService
